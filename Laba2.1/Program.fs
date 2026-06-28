68942630b4a49e7165797d6b265d47515babdde1
Samsheiko <sheiko22@mail.ru>
2026-06-29 01:31:38 +0500

    Add files via upload

---
+++ Laba2.1/Program.fs
@@ -0,0 +1,19 @@
+﻿
+open System
+
+// 1. Функция обработки 
+let checkEven numbers =
+    numbers |> List.map (fun x -> x % 2 = 0) //проверка тру или фолс
+
+// 2. Ввод и вывод (интерфейс)
+printf "Введите числа через пробел: "
+let input = Console.ReadLine()
+
+let result = 
+    input.Split(' ')               // Разрезаем строку по пробелам
+
+    |> Array.toList                // Превращаем в список
+    |> List.map int                // Превращаем текст в числа
+    |> checkEven                   // Вызываем нашу функцию
+
+printfn "%A" result
