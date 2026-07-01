36eead327d9bd747dc4fe0d6d589c6afd9ced70d
Samsheiko <sheiko22@mail.ru>
2026-07-01 20:35:44 +0500

    Add files via upload

---
+++ Laba2.1/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+let trfs x = x % 2 = 0
+
+[<EntryPoint>]
+let main args =
+    printf "Введите количество чисел:"
+    let n = int (Console.ReadLine())
+
+    let a =
+        [ for i in 1 .. n do
+            printf "Введите число %d: " i
+            let num = int (Console.ReadLine())
+            yield num ]
+
+    let res = List.map trfs a
+    printfn "Результат: %A" res
+    0
