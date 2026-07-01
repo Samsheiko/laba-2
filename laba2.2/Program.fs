65d438ce23ca24d4cd95020b2303037e337351c8
Samsheiko <sheiko22@mail.ru>
2026-07-01 20:33:00 +0500

    Add files via upload

---
+++ laba2.2/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let romanToDecimal roman =
+    match roman with
+    | "I" -> 1
+    | "II" -> 2
+    | "III" -> 3
+    | "IV" -> 4
+    | "V" -> 5
+    | "VI" -> 6
+    | "VII" -> 7
+    | "VIII" -> 8
+    | "IX" -> 9
+    | _ -> 0
+
+[<EntryPoint>]
+let main _ =
+    printf "Сколько римских чисел вводим? "
+    let n = int (Console.ReadLine())
+
+    let romanList =
+        [ for i in 1 .. n do
+            printf "Введите римское число %d (I-IX): " i
+            Console.ReadLine().Trim().ToUpper() ]
+
+    let totalSum = List.fold (fun acc roman -> acc + romanToDecimal roman) 0 romanList
+
+    printfn "Сумма в десятичной системе: %d" totalSum
+    0
